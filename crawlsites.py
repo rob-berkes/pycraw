af167fe47cc25a51b6436a702c59adeac94595cd
Rob Berkes <rob.berkes@gmail.com>
2014-12-17 19:19:33 +0000

    modding to use urllib2 for greater flexibility

--- crawlsites.py
+++ crawlsites.py
@@ -1,15 +1,30 @@
 import wget 
 import time
-  
+import urllib2
+import socket
+ 
+
+timeout = 8
+socket.setdefaulttimeout(timeout)
 
 ifp=open('54-149-p80.log','r')
 
 for line in ifp:
   line = line.strip().split()
   url = 'http://'+str(line[1])+'/'
-  time.sleep(3)
-  
+  req = urllib2.Request(url)
+  html = ''
   try:
-    wget.download(url,out='html/'+str(line[1])+'index.html')
-  except IOError:
-    continue
+    html = urllib2.urlopen(req)
+  except urllib2.HTTPError:
+    pass
+  except urllib2.URLError:
+    pass
+  
+  print "-======= site: "+str(line[1])+" =======-"
+  print html
+ 
+#  try:
+#    wget.download(url,out='html/'+str(line[1])+'index.html')
+#  except IOError:
+#    continue
