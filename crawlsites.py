daba5e846a5414a1eeed868f23ec6245c62a59cf
Rob Berkes <rob.berkes@gmail.com>
2014-12-17 19:34:27 +0000

    now crawls and adds to db in one

--- crawlsites.py
+++ crawlsites.py
@@ -2,13 +2,18 @@ import wget
 import time
 import urllib2
 import socket
- 
-
-timeout = 8
+import psycopg2 
+from bs4 import BeautifulSoup
+timeout = 5
 socket.setdefaulttimeout(timeout)
 
 ifp=open('54-149-p80.log','r')
-
+class Word:
+   url = ''
+   word = ''
+   position = 0
+conn= psycopg2.connect("dbname=words user=postgres")
+cur = conn.cursor()
 for line in ifp:
   line = line.strip().split()
   url = 'http://'+str(line[1])+'/'
@@ -23,6 +28,18 @@ for line in ifp:
   
   print "-======= site: "+str(line[1])+" =======-"
   print html
+  print "now indexing ..."
+  soup = BeautifulSoup(html)
+  full_text = soup.get_text()
+  full_text = full_text.strip().split()
+  COUNT=1
+  for word in full_text:
+    entry = Word()
+    entry.word = word.lower()
+    entry.position = COUNT
+    entry.url = url
+    cur.execute("INSERT INTO crawls (word, position, url) VALUES (%s, %s, %s)", (entry.word,entry.position,entry.url))
+    COUNT+=1
  
 #  try:
 #    wget.download(url,out='html/'+str(line[1])+'index.html')
