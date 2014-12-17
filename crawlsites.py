183305a99146008cd2faaef812f9f6d139d00859
Rob Berkes <rob.berkes@gmail.com>
2014-12-17 20:48:54 +0000

    exceptions

--- crawlsites.py
+++ crawlsites.py
@@ -4,16 +4,81 @@ import urllib2
 import socket
 import psycopg2 
 from bs4 import BeautifulSoup
+import re
+
 timeout = 5
 socket.setdefaulttimeout(timeout)
+
+exlink = re.compile(r'(http://)(.*)')
+exslink = re.compile(r'(https://)(.*)')
 
 ifp=open('54-149-p80.log','r')
 class Word:
    url = ''
    word = ''
    position = 0
+
 conn= psycopg2.connect("dbname=words user=postgres")
+conn.autocommit = True
 cur = conn.cursor()
+
+def procAllLinks(soup,url):
+  BASEURL = url
+  ofile = open('newlinks.log','a')
+  for link in soup.find_all('a'):
+    url = BASEURL
+    if re.match(exlink,link.get('href')) or re.match(exslink,link.get('href')):
+      try:
+        ofile.write(link.get('href'))
+        ofile.write('\n')
+      except TypeError:
+        pass
+      except UnicodeDecodeError:
+	pass
+      except UnicodeEncodeError:
+	pass
+    else:
+      url = BASEURL +str(link.get('href'))
+      req = urllib2.Request(url)
+      html = ''
+      try:
+        html = urllib2.urlopen(req)
+      except urllib2.HTTPError:
+        pass
+      except urllib2.URLError:
+        pass
+      full_text = soup.get_text()
+      full_text = full_text.strip().split()
+      COUNT=1
+      print "-**************Indexing Local link: ",
+      print url +" *************-"
+      print html
+      for word in full_text:
+        entry = Word()
+        entry.word = word.lower()
+        entry.position = COUNT
+        entry.url = url
+        cur.execute("INSERT INTO crawls (word, position, url) VALUES (%s, %s, %s)", (entry.word,entry.position,entry.url))
+        COUNT+=1
+  
+  url = url
+  req = urllib2.Request(url)
+  try:
+    html = urllib2.urlopen(req)
+  except:
+    pass
+  full_text = soup.get_text()
+  full_text = full_text.strip().split()
+  COUNT=1
+  for word in full_text:
+    entry = Word()
+    entry.word = word.lower()
+    entry.position = COUNT
+    entry.url = url
+    cur.execute("INSERT INTO crawls (word, position, url) VALUES (%s, %s, %s)", (entry.word,entry.position,entry.url))
+    COUNT+=1
+  ofile.close() 
+
 for line in ifp:
   line = line.strip().split()
   url = 'http://'+str(line[1])+'/'
@@ -25,21 +90,14 @@ for line in ifp:
     pass
   except urllib2.URLError:
     pass
+  except:
+    pass
   
   print "-======= site: "+str(line[1])+" =======-"
   print html
   print "now indexing ..."
   soup = BeautifulSoup(html)
-  full_text = soup.get_text()
-  full_text = full_text.strip().split()
-  COUNT=1
-  for word in full_text:
-    entry = Word()
-    entry.word = word.lower()
-    entry.position = COUNT
-    entry.url = url
-    cur.execute("INSERT INTO crawls (word, position, url) VALUES (%s, %s, %s)", (entry.word,entry.position,entry.url))
-    COUNT+=1
+  procAllLinks(soup,url)
  
 #  try:
 #    wget.download(url,out='html/'+str(line[1])+'index.html')
