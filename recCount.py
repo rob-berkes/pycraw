5bb4e1c9e48b54f9cad9382d7522df10b544a521
Rob Berkes <rob.berkes@gmail.com>
2014-12-17 05:02:38 +0000

    minor

--- recCount.py
+++ recCount.py
@@ -1,4 +1,21 @@
 import psycopg2
 conn = psycopg2.connect("dbname=words user=postgres")
 cur  = conn.cursor()
-print cur.execute("SELECT count(*) from crawls;")
+cur.execute("SELECT * from crawls;")
+print "Total words indexed: ",
+print cur.rowcount
+print "Instances of: "
+print "=============="
+cur.execute("SELECT * from crawls WHERE word='the';")
+print "the : ",
+print cur.rowcount
+cur.execute("SELECT * from crawls WHERE word='rob';")
+print "rob : ",
+print cur.rowcount
+cur.execute("SELECT * from crawls WHERE word='obama';")
+print "obama : ",
+print cur.rowcount
+cur.execute("SELECT * from crawls WHERE word='osama';")
+print "osama : ",
+print cur.rowcount
+
