fa4e1abe7bbac7c30d4808521f585756d4da69d6
Rob Berkes <rob.berkes@gmail.com>
2014-12-17 23:24:05 +0000

    maximum local links for infinite scroll sites

--- crawlsites.py
+++ crawlsites.py
@@ -6,7 +6,7 @@ import psycopg2
 from bs4 import BeautifulSoup
 from datetime import datetime
 import re
-
+MAXLOCALLINKCOUNT = 20
 timeout = 5
 socket.setdefaulttimeout(timeout)
 
@@ -27,43 +27,49 @@ def procAllLinks(soup,url):
   BASEURL = url
   ofile = open('newlinks.log','a')
   indexed = datetime.now()
+  LINKCOUNT = 0
   for link in soup.find_all('a'):
     url = BASEURL
-    if re.match(exlink,link.get('href')) or re.match(exslink,link.get('href')):
-      try:
-        ofile.write(link.get('href'))
-        ofile.write('\n')
-      except TypeError:
-        pass
-      except UnicodeDecodeError:
-	pass
-      except UnicodeEncodeError:
-	pass
-    else:
-      url = BASEURL +str(link.get('href'))
-      req = urllib2.Request(url)
-      html = ''
-      try:
-        html = urllib2.urlopen(req)
-      except urllib2.HTTPError:
-        pass
-      except urllib2.URLError:
-        pass
-      full_text = soup.get_text()
-      full_text = full_text.strip().split()
-      COUNT=1
-      print "-**************Indexing Local link: ",
-      print url +" *************-"
-      print html
-      cur.execute("INSERT INTO visits (url, indexed) VALUES (%s, %s)", (url, indexed))
-      for word in full_text:
-        entry = Word()
-        entry.word = word.lower()
-        entry.position = COUNT
-        entry.url = url
-        cur.execute("INSERT INTO crawls (word, position, url) VALUES (%s, %s, %s)", (entry.word,entry.position,entry.url))
-        COUNT+=1
-  
+    try:
+      if re.match(exlink,link.get('href')) or re.match(exslink,link.get('href')):
+        try:
+          ofile.write(link.get('href'))
+          ofile.write('\n')
+        except TypeError:
+          pass
+        except UnicodeDecodeError:
+	  pass
+        except UnicodeEncodeError:
+	  pass
+      else:
+        LINKCOUNT+=1
+        if LINKCOUNT > MAXLOCALLINKCOUNT:
+          break
+        url = BASEURL +str(link.get('href'))
+        req = urllib2.Request(url)
+        html = ''
+        try:
+          html = urllib2.urlopen(req)
+        except urllib2.HTTPError:
+          pass
+        except urllib2.URLError:
+          pass
+        full_text = soup.get_text()
+        full_text = full_text.strip().split()
+        COUNT=1
+        print "-**************Indexing Local link: ",
+        print url +" *************-"
+        print html
+        cur.execute("INSERT INTO visits (url, indexed) VALUES (%s, %s)", (url, indexed))
+        for word in full_text:
+          entry = Word()
+          entry.word = word.lower()
+          entry.position = COUNT
+          entry.url = url
+          cur.execute("INSERT INTO crawls (word, position, url) VALUES (%s, %s, %s)", (entry.word,entry.position,entry.url))
+          COUNT+=1
+    except TypeError:
+      pass
   url = url
   req = urllib2.Request(url)
   try:
@@ -84,10 +90,10 @@ def procAllLinks(soup,url):
 
 for line in ifp:
   line = line.strip().split()
-  hostn , aliasl, ipl = socket.gethostbyaddr(str(line[1]))
-  host = socket.getfqdn(hostn)
-  print host
-  quit()
+#  hostn , aliasl, ipl = socket.gethostbyaddr(str(line[1]))
+#  host = socket.getfqdn(hostn)
+#  print host
+#  quit()
   url = 'http://'+str(line[1])+'/'
   req = urllib2.Request(url)
   html = ''
