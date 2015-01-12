caeba47225acfd68712dfbadb9515b2d0f5a7c8d
Rob Berkes <rob.berkes@gmail.com>
2015-01-12 05:01:21 +0000

    store downloaded files into Hadoop

--- bin/masscan.py
+++ bin/masscan.py
@@ -1,6 +1,11 @@
 import subprocess
-anet=54
-for bnet in range(40,55):
-    IPADDR=str(anet)+'.'+str(bnet)+'.0.0/16'
-    OFILE='scans/'+str(anet)+'-'+str(bnet)+'--p80.log'
+import pydoop.hdfs as hdfs
+
+a_net=54
+hdfs.mkdir(a_net)
+for b_net in range(40,55):
+    IPADDR=str(a_net)+'.'+str(bnet)+'.0.0/16'
+    OFILE=str(a_net)+'-'+str(bnet)+'-p80.log'
     subprocess.call(['masscan','-p80','-oG',OFILE,IPADDR,'--rate=2000'])
+    HADOOP_FILE_NAME='scans/'+str(a_net)+'/'+OFILE
+    hdfs.put(OFILE
