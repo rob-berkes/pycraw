7549aa80e111510fcd1ad368b8bdc59a720324a3
Rob Berkes <rob.berkes@gmail.com>
2015-01-12 05:51:58 +0000

    store on local fs first, pause on masscan exit, then upload to hdfs.

--- bin/masscan.py
+++ bin/masscan.py
@@ -1,11 +1,14 @@
 import subprocess
 import pydoop.hdfs as hdfs
-
-a_net=54
-hdfs.mkdir(a_net)
-for b_net in range(40,55):
+import time
+for  a_net in range(54,56):
+  hdfs.mkdir('scans/'+str(a_net))
+  for bnet in range(144,155):
     IPADDR=str(a_net)+'.'+str(bnet)+'.0.0/16'
     OFILE=str(a_net)+'-'+str(bnet)+'-p80.log'
-    subprocess.call(['masscan','-p80','-oG',OFILE,IPADDR,'--rate=2000'])
+    A=subprocess.Popen(['masscan','-p80','-oG',OFILE,IPADDR])
+    A.wait()
+    time.sleep(2)
     HADOOP_FILE_NAME='scans/'+str(a_net)+'/'+OFILE
-    hdfs.put(OFILE
+    hdfs.put(OFILE,HADOOP_FILE_NAME)
+    subprocess.Popen(['rm',OFILE])
