30b1a9c509af063d54d690a36469711af9dfcea6
Rob Berkes <rob.berkes@gmail.com>
2015-01-15 20:30:38 +0000

    increase masscan scanning rate

--- bin/masscan.py
+++ bin/masscan.py
@@ -1,12 +1,12 @@
 import subprocess
 import pydoop.hdfs as hdfs
 import time
-for  a_net in range(54,56):
+for  a_net in range(124,133):
   hdfs.mkdir('scans/'+str(a_net))
-  for bnet in range(144,155):
+  for bnet in range(0,256):
     IPADDR=str(a_net)+'.'+str(bnet)+'.0.0/16'
     OFILE=str(a_net)+'-'+str(bnet)+'-p80.log'
-    A=subprocess.Popen(['masscan','-p80','-oG',OFILE,IPADDR])
+    A=subprocess.Popen(['masscan','-p80','-oG',OFILE,IPADDR,'--rate=2000'])
     A.wait()
     time.sleep(2)
     HADOOP_FILE_NAME='scans/'+str(a_net)+'/'+OFILE
