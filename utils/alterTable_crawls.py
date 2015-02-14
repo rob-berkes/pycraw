7d5b5ed5d606913045f03f6161bf4d8726f17e6c
Rob Berkes <rob.berkes@gmail.com>
2015-02-14 23:51:00 +0000

    removed unneeded utils from older postgres days

--- utils/alterTable_crawls.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn=psycopg2.connect("dbname=words user=postgres")
-cur = conn.cursor()
-cur.execute("ALTER TABLE crawls ADD linkid serial4;")
-conn.commit()
-conn.close()
--- utils/createTable_textlink.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn=psycopg2.connect("dbname=words user=postgres")
-cur = conn.cursor()
-cur.execute("CREATE TABLE textlinks ( linkid serial4, url text, fulltext text, PRIMARY KEY(linkid) );")
-conn.commit()
-conn.close()
