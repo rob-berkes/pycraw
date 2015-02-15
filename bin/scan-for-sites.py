cc885fb8317438f15fad30282adc91b06eb57c5f
Rob Berkes <rob.berkes@gmail.com>
2015-02-15 01:06:26 +0000

    integrated into hbase for continual scanning!

--- bin/scan-for-sites.py
+++ bin/scan-for-sites.py
@@ -2,10 +2,27 @@ from pywebhdfs.webhdfs import PyWebHdfsClient
 import happybase
 import subprocess
 import time
+from random import randint
+HBASE_NODE='data2'
 hdfs = PyWebHdfsClient(host='namenode',port='50070',user_name='root')
-conn=happybase.Connection('127.0.0.1')
-for  a_net in range(54,55):
+conn=happybase.Connection(HBASE_NODE)
+t=conn.table('anet')
+START=randint(1,255)
+for  a_net in range(START,START+1):
+  ROW=t.row(str(a_net))
+  if len(ROW) > 0:
+    for key, value in ROW.items():
+        if value != str(-1):
+          START=randint(1,255)
+	  continue
+  t.put(str(a_net),{'data:user':'thisnode'})
   for bnet in range(0,256):
+    if a_net==10:
+       continue
+    elif a_net==192 and bnet==168:
+       continue
+    elif a_net==172 and bnet==16:
+       continue
     IPADDR=str(a_net)+'.'+str(bnet)+'.0.0/16'
     OFILE=str(a_net)+'-'+str(bnet)+'-p80.log'
     A=subprocess.Popen(['masscan','-p80','-oG',OFILE,IPADDR,'--rate=2000'])
@@ -15,3 +32,4 @@ for  a_net in range(54,55):
     with open(OFILE) as ofp:
       hdfs.create_file(HADOOP_FILE_NAME,ofp)
     subprocess.Popen(['rm',OFILE])
+  t.put(str(a_net),{'data:user':'-1'})
